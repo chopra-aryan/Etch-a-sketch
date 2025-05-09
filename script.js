e5683224736d32e668f3ea1cde928b7291e0e9c1
Aryan <209628710+chopra-aryan@users.noreply.github.com>
2025-05-10 00:29:40 +0530

    Added comments for better understanding

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const container = document.querySelector('.container');
 
+// Adding event listener on Resize button to prompt user for the size of grid
+
 document.querySelector('.size-button').addEventListener('click', ()=> {
     const size = prompt("Enter the size of grid you want (N x N): ", 16);
     createGrid(size);  
@@ -8,10 +10,11 @@ document.querySelector('.size-button').addEventListener('click', ()=> {
 
 createGrid(16);
 
+// function which creates the Grid, according to the input from user
 function createGrid(size) {
 
-    document.documentElement.style.setProperty('--size', size);
-    container.innerHTML = '';
+    document.documentElement.style.setProperty('--size', size); //this line simply use inline styling to set the size attribute. documentElement implies <html></html>, after this line runs, it will look like <html style="size : size">
+    container.innerHTML = ''; //deleting current grid before generating new one
 
     for (i = 0; i < size*size; i++) {
         const div = document.createElement('div');
