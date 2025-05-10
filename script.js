300af33cc7c4ecd77df9ee58e5b2a8df65451a09
Aryan <209628710+chopra-aryan@users.noreply.github.com>
2025-05-10 17:39:04 +0530

    everything is working with those additional challenges :)

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const container = document.querySelector('.container');
 // Adding event listener on Resize button to prompt user for the size of grid
 
 document.querySelector('.size-button').addEventListener('click', ()=> {
-    const size = prompt("Enter the size of grid you want (N x N): ", 16);
-    createGrid(size);  
+    const size = Number(prompt("Enter the size of grid you want (N x N): ", 16));
+    if (size > 0 && size <=100 && Number.isInteger(size)){
+        createGrid(size); 
+    }
+    else {
+        alert("Enter a valid integer value between 1 and 100!");
+    }
+
+     
 })
 
 createGrid(16);
@@ -13,7 +20,7 @@ createGrid(16);
 // function which creates the Grid, according to the input from user
 function createGrid(size) {
 
-    document.documentElement.style.setProperty('--size', size); //this line simply use inline styling to set the size attribute. documentElement implies <html></html>, after this line runs, it will look like <html style="size : size">
+    document.querySelector('.container').style.setProperty('--size', size); //this line use inline styling to set the size attribute. documentElement implies <html></html>, after this line runs, it will look like <html style="size : size">
     container.innerHTML = ''; //deleting current grid before generating new one
 
     for (i = 0; i < size*size; i++) {
@@ -26,8 +33,25 @@ function createGrid(size) {
 
 function changeColorOnHover() {
     document.querySelectorAll('.items').forEach((item) => item.addEventListener('mouseenter', () => {
-        item.style.backgroundColor = 'black';
+        item.style.backgroundColor = getRandomColor();
+        item.style.opacity = 0.1;
+// code below increases opacity by 0.1 (10%) on each hover 
+        item.addEventListener('mouseenter', ()=> {
+            let currentOpacity = parseFloat(item.style.opacity);
+            if (currentOpacity < 1) {
+                item.style.opacity = Math.min(currentOpacity + 0.1, 1).toString(); // this makes sure that opacity doesn't exceed 1
+            }
+        })
     }));
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    
+
+    return `rgb(${r},${g},${b})`;
 }
 
 
